df79affc4b2da8554b7f47d55d9a1c80ac7b560c
abduamal <abduamal@ymail.com>
2020-09-15 19:59:49 -0500

    adds create function

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -4,6 +4,27 @@ class ProfessorsController < ApplicationController
   end
 
   def show
+    set_professor
+  end
+
+  def create
+    @professor = Professor.create(params.require(:professor).permit(:name, :department, :level))
+    redirect_to (@professor)
+  end
+
+  def new
+    @professor = Professor.new
+  end
+
+  def update
+
+  end
+
+
+
+  private
+
+  def set_professor
     @professor = Professor.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/professors', to: 'professors#index'
-  get '/professors/:id', to: 'professors#show', as: professor'
+  get '/professors/new', to: 'professors#new'
+  get '/professors/:id', to: 'professors#show', as: 'professor'
+  post '/professors', to: 'professors#create'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
